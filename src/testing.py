7159e4e09bd233267841652337e943cc5a6d3d56
Hassanosama <hassanosama007@gmail.com>
2020-08-01 21:29:59 +0200

    fixed patience & returned mse

--- src/testing.py
+++ src/testing.py
@@ -6,17 +6,24 @@ import random
 import time
 from functools import reduce
 import utils
+from featurizer import STFT, Spectrogram, calc_spectrogram
+
+
 
 if __name__ == "__main__":
     
-    #utils.convert_to_wav('D:/CMP/4th/GP/Test/Cheap thrills (Sia).mp3', 'D:/CMP/4th/GP/Test/Cheap thrills (Sia).wav')
+    utils.convert_to_wav('D:/CMP/4th/GP/Test/Cheap.mp3', 'D:/CMP/4th/GP/Test/Cheap.wav')
     #utils.convert_to_mp3('D:/CMP/4th/GP/Test/B[stem].wav', 'D:/CMP/4th/GP/Test/Sia [Vocals].mp3')
-
+    '''
     drums_track,_ = utils.audio_loader('D:/CMP/4th/GP/Test/Buitraker - Revo X/drums.wav')
     vocal_track,_ = utils.audio_loader('D:/CMP/4th/GP/Test/Buitraker - Revo X/vocals.wav')
 
-    utils.plot_signal(drums_track, title= 'time domain [drums]')
-    utils.plot_signal(vocal_track, title= 'time domain [vocals]')
+
+    utils.pretty_spectrogram(calc_spectrogram(drums_track), title='drums only')
+    utils.pretty_spectrogram(calc_spectrogram(vocal_track), title='vocal only')
+    '''
+    #utils.plot_signal(drums_track, title= 'time domain [drums]')
+    #utils.plot_signal(vocal_track, title= 'time domain [vocals]')
 
 
 
